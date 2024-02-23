f2bb3a76081f652cedc69e90fcab47e4b9fa0003
Hari8995 <hari8995p@gmail.com>
2024-02-23 13:25:42 +0530

    callback

---
+++ work2.js
@@ -0,0 +1,28 @@
+let text = "Please locate where 'locate' occurs!";
+
+// indexof
+let index = text.indexOf("locate");
+console.log(index);
+
+// lastindex
+console.log(text.lastIndexOf("locate"));
+
+// search
+console.log(text.search("locate"));
+
+
+// match
+console.log(text.match("ca"));
+
+
+// includes
+console.log(text.includes("where"));
+
+
+
+// startwith
+console.log(text.startsWith("Please"));
+
+
+
+
